290daae67e568ef73cd92202323f698a9ebb8e9f
Prajakta Patil <189462614+ppraju7709@users.noreply.github.com>
2026-02-15 18:13:07 +0530

    Add files via upload

---
+++ 01-basics/POITIVE-NEGATIVE-CHECKER/Positive_negative_checker.py
@@ -0,0 +1,7 @@
+a = int(input("Enter any number you want :"))
+if a == 0:
+    print(a," is equals to 0.")
+elif a>0:
+    print(a," is positive integer.")
+else:
+    print(a," is negative integer.")
